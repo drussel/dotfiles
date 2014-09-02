76a7195dcd15fdca27866671e12089fcf9046975
Daniel Russel <drussel@gmail.com>
2014-09-02 13:49:10 -0700

    add sublime 3 to setup

--- setup.sh
+++ setup.sh
@@ -35,3 +35,5 @@ ln -s src/dotfiles/Library/LaunchAgents/local.launchd.conf.plist Library/LaunchA
 cd ~/Library/Application\ Support
 trash Sublime\ Text\ 2
 ln -s ~/src/dotfiles/Library/Application\ Support/Sublime\ Text\ 2
+trash Sublime\ Text\ 3
+ln -s ~/src/dotfiles/Library/Application\ Support/Sublime\ Text\ 3
